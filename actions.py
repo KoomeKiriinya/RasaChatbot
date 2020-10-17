acb56e9e72d683b3e0f9eb05bf60c615d905d40f
Ian Koome <user@MacBooks-MacBook-Pro.local>
2020-10-17 11:27:03 +0300

    ++ happy removed

--- actions.py
+++ actions.py
@@ -68,17 +68,3 @@ class HealthForm(FormAction):
         dispatcher.utter_message("Thanks, great job!")
         return []
 
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
